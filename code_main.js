0555e202d22027fbd6b519b2f98e50f6e9b48ea7
Casdatt <josemgalc27@gmail.com>
2026-02-22 11:26:10 +0000

    Update code_main.js

--- code_main.js
+++ code_main.js
@@ -3,4 +3,4 @@ Achemj.textContent = `Achievements \u{1f929}`
 FGemj.textContent = `First Game \u{1f432}`;
 SGemj.textContent = `Second Game \u{1f43a}`;
 TGemj.textContent = `Third Game \u{1f988}`;
-FoGemoji.textContent = `Fourth Game \u{1faa8}`;
+FoGmji.textContent = `Fourth Game \u{1faa8}`;
