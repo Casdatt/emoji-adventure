7197f9c450f23c893278996550a612b29291fa36
Casdatt <josemgalc27@gmail.com>
2026-02-22 10:57:11 +0000

    Add files via upload
    Initial commit: Basic structure and main page

---
+++ code_main.js
@@ -0,0 +1,6 @@
+Skinsemj.textContent = `Skins \u{1f9d0}`
+Achemj.textContent = `Achievements \u{1f929}`
+FGemj.textContent = `First Game \u{1f432}`;
+SGemj.textContent = `Second Game \u{1f43a}`;
+TGemj.textContent = `Third Game \u{1f988}`;
+FoGemoji.textContent = `Fourth Game \u{1faa8}`;
