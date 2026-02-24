8cee8eda4aaa48bdbba0d4a21bde708f0544a9e6
Casdatt <josemgalc27@gmail.com>
2026-02-24 12:33:47 +0000

    Update code.js

--- code.js
+++ code.js
@@ -8,7 +8,7 @@ else {
 function MainCode() {
 	Skinsemj.textContent = `Skins \u{1f9d0}`
 	Achemj.textContent = `Achievements \u{1f929}`
-	FGemj.textContent = `First Game \u{1f432}`;
+	FGemj.textContent = `Green Dragon \u{1f432}`;
 	SGemj.textContent = `Second Game \u{1f43a}`;
 	TGemj.textContent = `Third Game \u{1f988}`;
 	FoGemj.textContent = `Fourth Game \u{1faa8}`;
@@ -381,3 +381,4 @@ function Scode() {
 		}
 	});
 }
+
